b05d23a3c79a865360c2154d3f08d5c7c90d42e0
Søren Berg Glasius <soeren@glasius.dk>
2019-04-09 19:46:43 +0200

    Initial commit

---
+++ grails-app/init/fields/one/to/many/Application.groovy
@@ -0,0 +1,10 @@
+package fields.one.to.many
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/fields/one/to/many/BootStrap.groovy
@@ -0,0 +1,9 @@
+package fields.one.to.many
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
